f886774755ec0469a9aa1a4f9527717bf820d382
am23mylcusd <132395346+am23mylcusd@users.noreply.github.com>
2023-05-02 12:35:26 -0700

    Update signease.py

--- signease.py
+++ signease.py
@@ -3,17 +3,16 @@ import streamlit as st
 def main():
     st.title("Sign In")
 
-    # Get the username from the user
-    username = st.text_input("Username")
-
-    # Check if the user has signed in before
-    if st.session_state.get("signed_in"):
-        st.write(f"Welcome back, {username}!")
-    else:
-        # If the user has not signed in before, display a sign-in button
+    # Get the username from the user or from the cookie
+    username = st.session_state.get("username")
+    if not username:
+        username = st.text_input("Username")
         if st.button("Sign In"):
-            st.session_state["signed_in"] = True
-            st.write(f"Welcome, {username}!")
+            st.session_state["username"] = username
+
+    # Display a welcome message to the user
+    if username:
+        st.write(f"Welcome back, {username}!")
 
 if __name__ == "__main__":
     main()
