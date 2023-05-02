291e0673a55d70c7343a2a93c16310fc2ea3e156
am23mylcusd <132395346+am23mylcusd@users.noreply.github.com>
2023-05-02 12:31:46 -0700

    Update signease.py

--- signease.py
+++ signease.py
@@ -1,52 +1,19 @@
 import streamlit as st
-import json
-import os
-
-def read_users():
-    if not os.path.exists('users.json'):
-        # Create a new file if it doesn't exist
-        with open('users.json', 'w') as f:
-            json.dump({}, f)
-    with open('users.json', 'r') as f:
-        try:
-            users = json.load(f)
-        except json.JSONDecodeError:
-            # Create a new file if the existing one is invalid
-            with open('users.json', 'w') as f:
-                json.dump({}, f)
-            users = {}
-    return users
-
-def write_users(users):
-    with open('users.json', 'w') as f:
-        json.dump(users, f)
-
-def login(username, password, users):
-    if username in users and users[username]['password'] == password:
-        return True
-    else:
-        return False
 
 def main():
-    st.title("Login Page")
-    st.write("Please enter your credentials to login.")
-    
-    # Get user input
-    username = st.text_input("Username")
-    password = st.text_input("Password", type="password")
-    
-    # Check if username and password are correct
-    if st.button("Login"):
-        users = read_users()
-        if login(username, password, users):
-            st.success("Login successful!")
-            # Create session
-            session_id = username
-            # Store session id in Streamlit's session state
-            st.session_state['session_id'] = session_id
-            # Redirect to your main application
-        else:
-            st.error("Invalid username or password")
+    st.title("Sign In")
 
-if __name__ == '__main__':
+    # Get the username from the user
+    username = st.text_input("Username")
+
+    # Check if the user has signed in before
+    if st.session_state.get("signed_in"):
+        st.write(f"Welcome back, {username}!")
+    else:
+        # If the user has not signed in before, display a sign-in button
+        if st.button("Sign In"):
+            st.session_state["signed_in"] = True
+            st.write(f"Welcome, {username}!")
+
+if __name__ == "__main__":
     main()
