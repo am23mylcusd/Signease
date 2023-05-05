9057aa4abc582fdcd6573326b6c98387c9e8fee7
am23mylcusd <132395346+am23mylcusd@users.noreply.github.com>
2023-05-05 11:55:20 -0700

    Update signease.py

--- signease.py
+++ signease.py
@@ -1,74 +1,62 @@
+Sure, I can help you with that. Here is an example of how to create a simple login page with cookies using "extra_streamlit_components" in Streamlit:
+
+```python
 import streamlit as st
-from streamlit.report_thread import get_report_ctx
-from streamlit.server.server import Server
+import extra_streamlit_components as stx
 
-# Define the login page
-def login():
-    st.title('Login')
+def create_login_page():
+    # Set up the login form fields
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
 
-    # Get the saved username from the SessionState (if it exists)
-    session_state = get_session()
-    saved_username = session_state.username
-
-    # Show a text input for the username
-    username = st.text_input('Username', saved_username)
-
-    # Show a password input for the password
-    password = st.text_input('Password', type='password')
-
-    # Show a "Remember Me" checkbox
-    remember_me = st.checkbox('Remember Me')
-
-    # Show a login button
-    if st.button('Login'):
-        # Check if the username and password are correct
-        if username == 'myusername' and password == 'mypassword':
-            # Set the session state with the username
-            session_state.username = username
-
-            # Redirect the user to the app page
-            url = get_server_url()
-            redirect_url = url + "?page=app&logged_in=True"
+    # Check if the form has been submitted
+    if st.form_submit_button("Log in"):
+        if username == "myusername" and password == "mypassword":
+            # Set a cookie to remember the user
+            stx.set_cookies({"user": username})
+            st.success("You have successfully logged in!")
+            # Redirect the user to the home page
             st.experimental_set_query_params(logged_in=True)
-            st.experimental_rerun()
-
         else:
-            # Show an error message
-            st.error('Incorrect username or password')
+            st.error("Invalid username or password")
 
-# Define the app page
-def app():
-    st.title('Welcome!')
+def create_home_page():
+    # Get the cookie for the user
+    user_cookie = stx.get_cookies().get("user")
 
-    # Show a welcome message to the logged in user
-    session_state = get_session()
-    username = session_state.username
-    st.write(f'Welcome, {username}!')
+    # Check if the user is logged in
+    if user_cookie is None:
+        # Redirect the user to the login page
+        st.experimental_set_query_params(logged_in=False)
+        return
 
-# Define the SessionState utility
-def get_session():
-    # Get the session ID
-    ctx = get_report_ctx()
-    session_id = ctx.session_id
+    # Display the home page
+    st.write(f"Welcome back, {user_cookie}!")
 
-    # Create a session state object for the current session
-    if not hasattr(ctx.session, 'my_state'):
-        ctx.session.my_state = SessionState()
-    return ctx.session.my_state
+def main():
+    # Set up the Streamlit app
+    st.set_page_config(page_title="Login Demo")
+    st.title("Login Demo")
 
-class SessionState:
-    def __init__(self):
-        self.username = None
+    # Determine which page to show based on the URL query parameters
+    logged_in = st.experimental_get_query_params().get("logged_in")
+    if logged_in == "True":
+        create_home_page()
+    else:
+        create_login_page()
 
-# Get the server URL
-def get_server_url():
-    server = Server.get_current()
-    if server is None:
-        raise RuntimeError("Not running with the Streamlit server")
-    return server.server_url
+if __name__ == "__main__":
+    main()
+```
 
-# Check if the user is logged in
-if st.experimental_get_query_params().get('logged_in'):
-    app()
-else:
-    login()
+Here's how the code works:
+
+1. The `create_login_page` function sets up a form with two fields, one for the username and one for the password. When the form is submitted, the function checks if the username and password are correct. If they are, it sets a cookie to remember the user and redirects them to the home page. If they are not, it displays an error message.
+
+2. The `create_home_page` function checks if there is a cookie for the user. If there is, it displays a welcome message. If there is not, it redirects the user back to the login page.
+
+3. The `main` function sets up the Streamlit app and determines which page to show based on the URL query parameters. If the user is logged in, it shows the home page. If the user is not logged in, it shows the login page.
+
+4. Finally, the `if __name__ == "__main__"` block ensures that the code is only executed if the file is run directly (i.e., not imported as a module).
+
+You can run this code in your terminal with the command `streamlit run app.py` (assuming that the code is saved in a file called `app.py`).
