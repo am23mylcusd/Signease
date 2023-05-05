a2ba9b8d1642df384b66d959cce02957b1b03ff3
am23mylcusd <132395346+am23mylcusd@users.noreply.github.com>
2023-05-05 12:27:46 -0700

    Update signease.py

--- signease.py
+++ signease.py
@@ -1,5 +1,28 @@
 import streamlit as st
+from streamlit_cookies_manager import EncryptedCookieManager
 
+# This should be on top of your script
+cookies = EncryptedCookieManager(
+    # This prefix will get added to all your cookie names.
+    # This way you can run your app on Streamlit Cloud without cookie name clashes with other apps.
+    prefix="localhost/",
+    #prefix="",   # no prefix will show all your cookies for this domain
+    # You should setup COOKIES_PASSWORD secret if you're running on Streamlit Cloud.
+    #password=os.environ.get("COOKIES_PASSWORD", "My secret password"),
+    password='changeme'
+)
+
+if not cookies.ready():
+    # Wait for the component to load and send us current cookies.
+    st.stop()
+
+st.write("Current cookies:", cookies)
+name = st.text_input("New name for a cookie (key)")
+value = st.text_input("New value for a cookie (value)")
+if st.button("Change the cookie, will save on next rerun"):
+    cookies[name] = value  # This will get saved on next rerun
+    if st.button("Force saving cookies now, without rerun"):
+        cookies.save()  # Force saving the cookies now, without a rerun
 # Set a key for the cookie
 COOKIE_KEY = "logged_in"
 
