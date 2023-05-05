abb892151f825dd51c9047bafd317850d2b5dab4
am23mylcusd <132395346+am23mylcusd@users.noreply.github.com>
2023-05-05 11:50:03 -0700

    Update signease.py

--- signease.py
+++ signease.py
@@ -1,18 +1,14 @@
 import streamlit as st
-
-# Set up the cookie for storing login information
-def set_user_cookie(username):
-    if username:
-        st.set_cookie('user', username)
-    else:
-        st.set_cookie('user', '', expires_at=0)
+from streamlit.hashing import _CodeHasher
+from streamlit.report_thread import get_report_ctx
 
 # Define the login page
 def login():
     st.title('Login')
 
-    # Get the saved username from the cookie (if it exists)
-    saved_username = st.get_cookie('user')
+    # Get the saved username from the SessionState (if it exists)
+    session_state = get_session()
+    saved_username = session_state.username
 
     # Show a text input for the username
     username = st.text_input('Username', saved_username)
@@ -27,8 +23,8 @@ def login():
     if st.button('Login'):
         # Check if the username and password are correct
         if username == 'myusername' and password == 'mypassword':
-            # Set the user cookie
-            set_user_cookie(username)
+            # Set the session state with the username
+            session_state.username = username
 
             # Redirect the user to the app page
             st.experimental_set_query_params(logged_in=True)
@@ -41,8 +37,23 @@ def app():
     st.title('Welcome!')
 
     # Show a welcome message to the logged in user
-    username = st.get_cookie('user')
+    session_state = get_session()
+    username = session_state.username
     st.write(f'Welcome, {username}!')
+
+# Define the SessionState utility
+def get_session():
+    # Get the session ID hash
+    ctx = get_report_ctx()
+    current_code_hash = _CodeHasher.get_session_id_hash()
+
+    # Create a session state object for the current session
+    if not hasattr(ctx, 'session_state'):
+        ctx.session_state = _get_state(session=current_code_hash)
+    return ctx.session_state
+
+def _get_state(**kwargs):
+    return st._get_state(**kwargs)
 
 # Check if the user is logged in
 if st.experimental_get_query_params().get('logged_in'):
