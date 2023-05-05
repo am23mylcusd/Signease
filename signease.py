d886954e49ec6050ce5ba4075ce545846fd8675e
am23mylcusd <132395346+am23mylcusd@users.noreply.github.com>
2023-05-05 11:55:51 -0700

    Update signease.py

--- signease.py
+++ signease.py
@@ -1,6 +1,3 @@
-Sure, I can help you with that. Here is an example of how to create a simple login page with cookies using "extra_streamlit_components" in Streamlit:
-
-```python
 import streamlit as st
 import extra_streamlit_components as stx
 
@@ -47,16 +44,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-```
-
-Here's how the code works:
-
-1. The `create_login_page` function sets up a form with two fields, one for the username and one for the password. When the form is submitted, the function checks if the username and password are correct. If they are, it sets a cookie to remember the user and redirects them to the home page. If they are not, it displays an error message.
-
-2. The `create_home_page` function checks if there is a cookie for the user. If there is, it displays a welcome message. If there is not, it redirects the user back to the login page.
-
-3. The `main` function sets up the Streamlit app and determines which page to show based on the URL query parameters. If the user is logged in, it shows the home page. If the user is not logged in, it shows the login page.
-
-4. Finally, the `if __name__ == "__main__"` block ensures that the code is only executed if the file is run directly (i.e., not imported as a module).
-
-You can run this code in your terminal with the command `streamlit run app.py` (assuming that the code is saved in a file called `app.py`).
