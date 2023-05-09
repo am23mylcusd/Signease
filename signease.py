91eb0d6ca498ddae2138bd30e8fecc88150417d0
am23mylcusd <132395346+am23mylcusd@users.noreply.github.com>
2023-05-09 12:39:13 -0700

    Update signease.py

--- signease.py
+++ signease.py
@@ -16,29 +16,33 @@ if not cookies.ready():
     # Wait for the component to load and send us current cookies.
     st.stop()
 
-st.write("Current cookies:", cookies)
-name = st.text_input("New name for a cookie (key)")
-value = st.text_input("New value for a cookie (value)")
-if st.button("Change the cookie, will save on next rerun"):
-    cookies[name] = value  # This will get saved on next rerun
-    if st.button("Force saving cookies now, without rerun"):
-        cookies.save()  # Force saving the cookies now, without a rerun
 # Set a key for the cookie
 COOKIE_KEY = "logged_in"
 
+def signup():
+    name = st.text_input("Name")
+    last_name = st.text_input("Last Name")
+    email = st.text_input("School Email")
+    if st.button("Sign Up!"):
+        st.session_state[COOKIE_KEY] = True
+        cookies[name] = {last_name,email,st.session_state[COOKIE_KEY]}
+        cookies.save()
+
+
 # Define a function to check if the user is logged in
 def is_logged_in():
-    
-    return st.session_state.get(COOKIE_KEY)
+    return cookies[name][2]
 
 # Define the login form
 def login():
-    username = st.text_input("Username")
-    password = st.text_input("Password", type="password")
+    name = st.text_input("Name")
+    last_name = st.text_input("Last Name")
+    email = st.text_input("School Email")
     if st.button("Login"):
-        if username in cookies and password == cookies[username]:
+        if name in cookies and last_name == cookies[username][0] and email == cookies[username][1]:
             # Set the cookie to True
             st.session_state[COOKIE_KEY] = True
+            cookies[name][2] = st.session_state[COOKIE_KEY]
             # Redirect to the homepage
             # st.experimental_rerun()
         else:
@@ -48,8 +52,23 @@ def login():
 def logout():
     # Remove the cookie
     st.session_state[COOKIE_KEY] = False
+    cookies[name][2] = st.session_state[COOKIE_KEY]
     # Redirect to the login page
-    st.experimental_rerun()
+    st.experimental_rerun()       
+        
+        
+ 
+st.button("Sign up", on_click=signup)
+
+
+# st.write("Current cookies:", cookies)
+# name = st.text_input("New name for a cookie (key)")
+# value = st.text_input("New value for a cookie (value)")
+# if st.button("Change the cookie, will save on next rerun"):
+#     cookies[name] = value  # This will get saved on next rerun
+#     if st.button("Force saving cookies now, without rerun"):
+#         cookies.save()  # Force saving the cookies now, without a rerun
+
 
 # Check if the user is logged in
 if is_logged_in():
