0dfa6ed95b5c012a6cfc90f0120feb4dbeab0e51
am23mylcusd <132395346+am23mylcusd@users.noreply.github.com>
2023-05-11 11:43:53 -0700

    Update signease.py

--- signease.py
+++ signease.py
@@ -54,7 +54,7 @@ def logout():
     # Remove the cookie
     st.session_state[COOKIE_KEY] = False
     del cookies["alex"]
-    cookies["alex"] = st.session_state[COOKIE_KEY]
+    cookies["alex"] = {"","".session_state[COOKIE_KEY]}
     # Redirect to the login page
     st.experimental_rerun()       
         
