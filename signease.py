6eeb79c77059d4a76c4d814195cf84e88da66216
am23mylcusd <132395346+am23mylcusd@users.noreply.github.com>
2023-05-23 22:04:56 -0700

    Update signease.py

--- signease.py
+++ signease.py
@@ -3,28 +3,29 @@ import streamlit as st
 
 COMPLETION_KEY = "Thanks"
 
-name = st.text_input("Name")
-last_name = st.text_input("Last Name")
-id = st.text_input("Student ID")
-checkbox_input = st.checkbox('Signing Out?', key='my_checkbox')
-if st.session_state.my_checkbox == True:
-    with st.form(key='my_form'):
-        st.radio("Where are you going?",('Office', 'Bathroom', 'meeting'), key='choice')
-        submit_button = st.form_submit_button(label='Submit')
-        if submit_button:
-            st.write(st.session_state.choice)
-            with open('logs.txt', 'w') as f:
-                f.write( '%(asctime)s %(name)s %(last_name)s %(id)s %(st.session_state.choice)s')
-            st.session_state[COMPLETION_KEY] = True
-else:
-    submit_button = st.button("Submit")
-    if submit_button:
-        with open('logs.txt', 'w') as f:
-            f.write('%(asctime)s %(name)s %(last_name)s %(id)s returned to class')
-        st.session_state[COMPLETION_KEY] = True
 
 if st.session_state.get(COMPLETION_KEY, False):
     st.write("Thanks")
+else:
+    name = st.text_input("Name")
+    last_name = st.text_input("Last Name")
+    id = st.text_input("Student ID")
+    checkbox_input = st.checkbox('Signing Out?', key='my_checkbox')
+    if st.session_state.my_checkbox == True:
+        with st.form(key='my_form'):
+            st.radio("Where are you going?",('Office', 'Bathroom', 'meeting'), key='choice')
+            submit_button = st.form_submit_button(label='Submit')
+            if submit_button:
+                st.write(st.session_state.choice)
+                with open('logs.txt', 'w') as f:
+                    f.write( '%(asctime)s %(name)s %(last_name)s %(id)s %(st.session_state.choice)s')
+                st.session_state[COMPLETION_KEY] = True
+    else:
+        submit_button = st.button("Submit")
+        if submit_button:
+            with open('logs.txt', 'w') as f:
+                f.write('%(asctime)s %(name)s %(last_name)s %(id)s returned to class')
+            st.session_state[COMPLETION_KEY] = True
 # import json
 # import streamlit as st
 # from streamlit_cookies_manager import EncryptedCookieManager
